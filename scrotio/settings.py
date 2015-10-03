33dfbe8245e96ff4e7aa8980165cc581e9cc1ff6
Patrick Allen <prallen90@gmail.com>
2015-10-02 23:01:58 -0400

    Configure django to use postgresql

--- scrotio/settings.py
+++ scrotio/settings.py
@@ -75,14 +75,16 @@ WSGI_APPLICATION = 'scrotio.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'scrotiodb',
+        'USER': 'username',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.8/topics/i18n/
