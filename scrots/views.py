a972701319fa3b70fe0b4a49e812f37966453e17
Patrick Allen <prallen90@gmail.com>
2015-10-04 11:32:21 -0400

    Add usability to home page form

--- scrots/views.py
+++ scrots/views.py
@@ -2,12 +2,22 @@ from django.views.generic.edit import FormView
 from django.views.generic import ListView, DetailView
 from scrots.forms import UrlScrotForm
 from .models import Scrot
+from wappa.pages import get_screenshot_from_url
 
 
 class HomePageView(FormView):
     form_class = UrlScrotForm
     template_name = 'scrots/index.html'
-    success_url = '/'
+
+    def form_valid(self, form):
+        print("Form was valid")
+        url = form.data['url']
+        self.scrot = get_screenshot_from_url(url)
+        return super(HomePageView, self).form_valid(form)
+
+    def get_success_url(self):
+        url = '/scrot/{}'.format(self.scrot.id)
+        return url
 
 
 class RecentScrotsView(ListView):
