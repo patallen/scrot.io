b4b4e51bde8431687af03a09352bd359b0ce9475
Patrick Allen <prallen90@gmail.com>
2015-10-06 14:24:08 +0000

    Add django-crispy-forms

--- scrotio/settings.py
+++ scrotio/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-
+    'crispy_forms',
     'scrots',
 )
 
@@ -107,3 +107,5 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap3'
--- scrots/forms.py
+++ scrots/forms.py
@@ -1,5 +1,11 @@
 from django import forms
+from crispy_forms.helper import FormHelper
 
 
 class UrlScrotForm(forms.Form):
     url = forms.CharField(required=True)
+
+    def __init__ (self, *args, **kwargs):
+        self.helper = FormHelper()
+        self.helper.form_show_labels = False
+        super(UrlScrotForm, self).__init__(*args, **kwargs)
