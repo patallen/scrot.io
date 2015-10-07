bb8f9f15780e0fc0a4eb20f2a37f8d3a25136709
Patrick Allen <prallen90@gmail.com>
2015-10-06 21:05:33 -0400

    Refactor ScrotHandler to use PIL.Image instead of filepaths

--- screenshotter/handlers.py
+++ screenshotter/handlers.py
@@ -3,6 +3,7 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from urllib.parse import urlparse, urlunparse
 from datetime import datetime
 import os
+import io
 from PIL import Image
 
 from django.conf import settings
@@ -46,35 +47,33 @@ class ScrotHandler:
     def _get_fn(self, postfix):
         return '{}_{}.{}'.format(self.basefn, postfix, self.ext)
 
-    def create_scrot(self):
+    def load(self):
         if not os.path.exists(settings.MEDIA_ROOT):
             os.makedirs(settings.MEDIA_ROOT)
 
         self.driver.set_window_size(width=self.width, height=self.height)
         self.driver.get(self.url)
         filepath = os.path.join(settings.MEDIA_ROOT, self._get_fn('full'))
-        self.driver.save_screenshot(filepath)
+        stream = io.BytesIO(self.driver.get_screenshot_as_png())
+        self.screenshot = Image.open(stream)
+        self.screenshot.save(filepath)
 
-    def crop_to_screen(self):
-        filepath = os.path.join(self.base_path, self._get_fn('full'))
-        img = Image.open(filepath)
-        img_width, _ = img.size
-        cropped_img = img.crop((0, 0, img_width, self.height))
+    def crop(self):
+        self.cropped = self.screenshot.crop((0, 0, self.width, self.height))
         new_filepath = os.path.join(self.base_path, self._get_fn('screen'))
-        cropped_img.save(new_filepath)
+        self.cropped.save(new_filepath)
 
-    def create_thumbnail(self):
-        filepath = os.path.join(self.base_path, self._get_fn('screen'))
-        img = Image.open(filepath)
-        img.thumbnail((640, 360))
-        img.thumbnail((320, 180), Image.ANTIALIAS)
+    def thumb(self):
+        self.thumb = self.cropped
+        self.thumb.thumbnail((640, 360))
+        self.thumb.thumbnail((320, 180), Image.ANTIALIAS)
         filepath = os.path.join(self.base_path, self._get_fn('thumb'))
-        img.save(filepath)
+        self.thumb.save(filepath)
 
     def create_images(self):
-        self.create_scrot()
-        self.crop_to_screen()
-        self.create_thumbnail()
+        self.load()
+        self.crop()
+        self.thumb()
 
     def save_to_db(self):
         scrot = Scrot.objects.create(
