321454a431d6d3d0095e54bb2becfebb15fe6d1f
Patrick Allen <prallen90@gmail.com>
2015-10-08 22:57:30 -0400

    Clean up URL form with crispyforms helper

--- scrots/forms.py
+++ scrots/forms.py
@@ -1,11 +1,16 @@
 from django import forms
 from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Div, Submit
 
 
 class UrlScrotForm(forms.Form):
     url = forms.CharField(required=True)
 
-    def __init__ (self, *args, **kwargs):
+    def __init__(self, *args, **kwargs):
+        super(UrlScrotForm, self).__init__(*args, **kwargs)
         self.helper = FormHelper()
         self.helper.form_show_labels = False
-        super(UrlScrotForm, self).__init__(*args, **kwargs)
+        self.helper.layout = Layout(
+            Div("url", css_class="col-lg-10"),
+            Submit('submit', 'Scrot It', css_class="col-lg-2")
+        )
