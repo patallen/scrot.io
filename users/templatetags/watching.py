5457433ed46b688e2ed236fe8824602cf649191a
Patrick Allen <prallen90@gmail.com>
2015-10-12 15:33:47 +0000

    Handle anonymous users in user_is_watching templatetag

--- users/templatetags/watching.py
+++ users/templatetags/watching.py
@@ -6,13 +6,20 @@ register = template.Library()
 
 @register.simple_tag(takes_context=True)
 def user_is_watching(context, website):
+    """
+    Template tag that returns the HTML for the watching icon for Websites.
+    """
     user = context['request'].user
     watching_html = (
         '<a href="" title="{}">'
         '<i class="glyphicon glyphicon-eye-open {}">'
         '</i>'
     )
-    if user.likes_website(website):
-        return watching_html.format("Watching", settings.WATCHING_CLASS)
+    if user.is_authenticated():
+        # If the user is not anonymous, find out if she is watching the Website
+        if user.likes_website(website):
+            return watching_html.format("Watching", settings.WATCHING_CLASS)
+        else:
+            return watching_html.format("Start Watching", '')
     else:
-        return watching_html.format("Start Watching", '')
+        return ''
