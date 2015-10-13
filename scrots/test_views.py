bdf03c3c54461239d1e84d5e843d72b608553a5d
Patrick Allen <prallen90@gmail.com>
2015-10-13 16:55:45 +0000

    Make current view tests work with new models

--- scrots/test_views.py
+++ scrots/test_views.py
@@ -1,33 +1,37 @@
+import tempfile
+
 from django.test import TestCase
-from scrots.models import Scrot
+from scrots.models import Snapshot, Website
 from django.core.urlresolvers import reverse
+from django.conf import settings
 
 
 class ScrotsViewsTestCase(TestCase):
     def setUp(self):
-        Scrot.objects.get_or_create(domain="google.com",
-                                    scrot_file="google.png",
-                                    width=1600, height=900)
-        Scrot.objects.get_or_create(domain="abc.com",
-                                    scrot_file="abc.png",
-                                    width=1600, height=900)
+        settings.MEDIA_ROOT = tempfile.mkdtemp()
+        google, _= Website.objects.get_or_create(domain="google.com")
+        stackoverflow, _= Website.objects.get_or_create(domain="stackoverflow.com")
+        google.add_snapshot()
+        stackoverflow.add_snapshot()
 
     def test_index(self):
         res = self.client.get('/')
+        ctx = res.context
         form_url = 'id="id_url" name="url" type="text"'
         self.assertEquals(res.status_code, 200)
         self.assertTemplateUsed(res, 'scrots/index.html')
         self.assertContains(res, 'Scrot.io')
         self.assertContains(res, form_url)
+        self.assertEquals(len(ctx['object_list']), 2)
 
     def test_recent_list(self):
         res = self.client.get(reverse("recent_list"))
         ctx = res.context
+        for x in ctx['object_list']: print(x)
         self.assertEquals(res.status_code, 200)
         self.assertEquals(len(ctx['object_list']), 2)
 
     def test_detail(self):
-        res = self.client.get(reverse("scrot_detail", kwargs={'pk': '1'}))
+        res = self.client.get(reverse("website_detail", kwargs={'pk': '1'}))
         ctx = res.context
-        self.assertContains(res, 'google.png')
-        self.assertIn(ctx['object'].domain, 'google.com')
+        self.assertEqual(ctx['object'].domain, 'google.com')
